e3deb17d96332fb35831c3671abc83b8181c2b43
KujouKarenKwi <115976160+KujouKarenKwi@users.noreply.github.com>
2025-12-10 19:48:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-#
-# [https://github.com/P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
-# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# 取消注释 helloworld (通常是 SSR+)
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 添加 Passwall 源 (强烈推荐)
+# 1. 添加 Passwall 源 (依赖和主程序)
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# 如果需要 OpenClash，可以取消下面这行的注释
+# 2. 添加 OpenClash 源
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+
+# 3. 添加 Argon 主题源码 (Jerrykuku版本，比官方源更好看且支持自定义)
+# 先移除可能存在的旧 argon 引用，防止冲突
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+# 4. 添加 N1 专用 Amlogic 写入工具 (Ophub版本，用于写入 EMMC)
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic

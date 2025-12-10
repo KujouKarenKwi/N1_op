823e6a815ef08cae97d1e1776320c83fc9689abd
KujouKarenKwi <115976160+KujouKarenKwi@users.noreply.github.com>
2025-12-10 19:49:13 +0800

    Update diy-part2.sh for network configuration
    Refactor OpenWrt DIY script for network and firewall settings.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-#
-# [https://github.com/P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
-# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# 1. 修改默认 IP 为 10.10.10.10 (这是第一层保障)
+# 1. 基础修正
+# 修改默认 IP
 sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
-
-# 2. 修改主机名为 ImmortalWrt-Side (可选，方便识别)
-sed -i 's/OpenWrt/ImmortalWrt-Side/g' package/base-files/files/bin/config_generate
-
-# 3. 设置默认主题为 Argon (ImmortalWrt 通常自带 Argon，设为默认更好看)
+# 修改主机名
+sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+# 设置默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 4. 创建自定义配置脚本 (核心部分：配置网关、DNS、关闭DHCP、关闭IPv6)
-# 这个脚本会在系统第一次启动时执行
+# 2. 核心网络配置 (uci-defaults)
 cat > package/base-files/files/etc/uci-defaults/99-custom-settings <<EOF
-# --- 网络基础配置 ---
+# --- 网络配置 ---
 uci set network.lan.proto='static'
 uci set network.lan.ipaddr='10.10.10.10'
 uci set network.lan.netmask='255.255.255.0'
 uci set network.lan.gateway='10.10.10.1'
-uci set network.lan.dns='10.10.10.1'
+# 设置两个 DNS
+uci set network.lan.dns='10.10.10.1 223.5.5.5'
+
+# --- 旁路由防火墙修正 (重要) ---
+# 添加自定义防火墙规则，解决部分设备无法上网问题
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> /etc/firewall.user
 
 # --- 关闭 DHCP ---
-# 旁路由必须关闭 DHCP，否则会干扰主路由
 uci set dhcp.lan.ignore='1'
 
-# --- 关闭 IPv6 ---
-# 删除 IPv6 相关接口和 DHCPv6 设置
+# --- 关闭 IPv6 (彻底禁用) ---
 uci set network.lan.ipv6='0'
 uci set dhcp.lan.dhcpv6='disabled'
 uci delete dhcp.lan.ra
@@ -37,13 +34,15 @@ uci delete dhcp.lan.ra_service
 uci delete dhcp.lan.ndp
 uci delete network.globals.ula_prefix
 
-# 提交网络和DHCP修改
+# --- Docker 优化 ---
+# 自动挂载 Docker 数据分区 (如果 N1 已扩容分区)
+uci set dockerd.globals.data_root='/opt/docker'
+
+# 提交修改
 uci commit network
 uci commit dhcp
-
-# --- 禁用系统级 IPv6 服务 ---
+uci commit dockerd
 /etc/init.d/odhcpd disable
 /etc/init.d/odhcpd stop
-
 exit 0
 EOF

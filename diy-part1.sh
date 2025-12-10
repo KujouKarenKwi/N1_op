8c090a091f2e9ac7325fbafddf5096ff5270f292
KujouKarenKwi <115976160+KujouKarenKwi@users.noreply.github.com>
2025-12-10 19:39:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
+# [https://github.com/P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 取消注释 helloworld (通常是 SSR+)
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 添加 Passwall 源 (强烈推荐)
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 如果需要 OpenClash，可以取消下面这行的注释
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default

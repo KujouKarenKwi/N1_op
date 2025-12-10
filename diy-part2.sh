5d64e42b8a5f4006f66693de84ed508169dbe89f
KujouKarenKwi <115976160+KujouKarenKwi@users.noreply.github.com>
2025-12-10 19:44:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
+# [https://github.com/P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改默认 IP 为 10.10.10.10 (这是第一层保障)
+sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 修改主机名为 ImmortalWrt-Side (可选，方便识别)
+sed -i 's/OpenWrt/ImmortalWrt-Side/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 设置默认主题为 Argon (ImmortalWrt 通常自带 Argon，设为默认更好看)
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 4. 创建自定义配置脚本 (核心部分：配置网关、DNS、关闭DHCP、关闭IPv6)
+# 这个脚本会在系统第一次启动时执行
+cat > package/base-files/files/etc/uci-defaults/99-custom-settings <<EOF
+# --- 网络基础配置 ---
+uci set network.lan.proto='static'
+uci set network.lan.ipaddr='10.10.10.10'
+uci set network.lan.netmask='255.255.255.0'
+uci set network.lan.gateway='10.10.10.1'
+uci set network.lan.dns='10.10.10.1'
+
+# --- 关闭 DHCP ---
+# 旁路由必须关闭 DHCP，否则会干扰主路由
+uci set dhcp.lan.ignore='1'
+
+# --- 关闭 IPv6 ---
+# 删除 IPv6 相关接口和 DHCPv6 设置
+uci set network.lan.ipv6='0'
+uci set dhcp.lan.dhcpv6='disabled'
+uci delete dhcp.lan.ra
+uci delete dhcp.lan.ra_service
+uci delete dhcp.lan.ndp
+uci delete network.globals.ula_prefix
+
+# 提交网络和DHCP修改
+uci commit network
+uci commit dhcp
+
+# --- 禁用系统级 IPv6 服务 ---
+/etc/init.d/odhcpd disable
+/etc/init.d/odhcpd stop
+
+exit 0
+EOF
